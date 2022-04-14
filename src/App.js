4970eb80d94dbb52814852d49c271f96b210a654
VP <huuwulleop@hotmail.com>
2022-04-14 12:14:49 -0400

    Add filtering countries

--- src/App.js
+++ src/App.js
@@ -23,13 +23,23 @@ const App = () => {
     console.log(event.target.value);
     setSearchFilter(event.target.value)
   }
-  
+
+  const filteredCountries = countries.filter(country => (
+    country.name.common.includes(searchFilter)
+  ))
+
+  console.log(filteredCountries);
+
   return (
     <div>
       <SearchForm searchFilter={searchFilter} changeSearchFilter={changeSearchFilter} />
-      {countries.length > 10
+      {filteredCountries.length > 10
         ? 'Too many to list'
-        : 'Display here'
+        : (
+          <ul>
+            {filteredCountries.map(fC => <li>{fC.name.common}</li>)}
+          </ul>
+        )
       }
     </div>
   )
